c71cb5167a906a60ac19b4e543be39eadb17cc01
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-08-26 21:57:48 +0100

    working on default pending and error components

--- packages/vue-router/src/index.tsx
+++ packages/vue-router/src/index.tsx
@@ -1,6 +1,16 @@
-import { inject, InjectionKey } from 'vue'
+import { inject, InjectionKey, ref } from 'vue'
+import { RegisteredRouter } from "@tanstack/router-core"
+import warning from 'tiny-warning'
 
 const matchIdsContext = Symbol() as InjectionKey<string[]>
+const routerContext = Symbol() as InjectionKey<RegisteredRouter>
+
+export function useRouter(): RegisteredRouter {
+    const value = inject(routerContext)
+
+    warning(value, 'useRouter must be used inside a <Router> component!')
+    return value
+  }
 
 export function Outlet() {
     const matchIds = inject(matchIdsContext)!.slice(1)
@@ -11,6 +21,50 @@ export function Outlet() {
 
   return <Match matchIds={matchIds} />
 }
+
+const defaultPending = () => null
+
+export function ErrorComponent({ error }: { error: any }) {
+    const [show, setShow] = ref(process.env.NODE_ENV !== 'production')
+  
+    return (
+      <div style={{ padding: '.5rem', maxWidth: '100%' }}>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '.5rem' }}>
+          <strong style={{ fontSize: '1rem' }}>Something went wrong!</strong>
+          <button
+            style={{
+              appearance: 'none',
+              fontSize: '.6em',
+              border: '1px solid currentColor',
+              padding: '.1rem .2rem',
+              fontWeight: 'bold',
+              borderRadius: '.25rem',
+            }}
+            onClick={() => setShow((d) => !d)}
+          >
+            {show ? 'Hide Error' : 'Show Error'}
+          </button>
+        </div>
+        <div style={{ height: '.25rem' }} />
+        {show ? (
+          <div>
+            <pre
+              style={{
+                fontSize: '.7em',
+                border: '1px solid red',
+                borderRadius: '.25rem',
+                padding: '.3rem',
+                color: 'red',
+                overflow: 'auto',
+              }}
+            >
+              {error.message ? <code>{error.message}</code> : null}
+            </pre>
+          </div>
+        ) : null}
+      </div>
+    )
+  }
 
 function Match({ matchIds }: { matchIds: string[] }) {
     const router = useRouter()
